d9d75c6aca0129c1e1f4910db0ecc743151c4314
Mustafa Keser <keserzade@gmail.com>
2024-12-15 22:08:55 +0300

    completed

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -18,22 +18,21 @@ const Hero = () => {
   }, []);
   return (
     <div
-      className="h-[60vh] relative -z-10 flex justify-center items-center"
-      style={{
+      className="h-[60vh] relative -z-50 flex justify-center items-center"
+/*      style={{
         backgroundImage: `url(${imageURL + movie?.backdrop_path})`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat',
         backgroundBlendMode: 'overlay',
-      }}
+      }}*/
     >
+        <img className={'object-cover w-full h-full'} src={`${imageURL + movie?.backdrop_path}`} alt=""/>
       {/*<div className="h-full w-full bg-[linear-gradient(180deg,rgba(31,41,55,0.1)60%,rgba(251,248,239,1)100%)] bottom-0 absolute"></div>*/}
       <div className="banner-ln"></div>
-   
-  
       <div className="absolute inset-0 bg-[var(--dark-blue)] blur-3xl opacity-60 max-sm:hidden"></div>
 
-      <h1 className="text-lg md:text-2xl lg:text-3xl text-white z-30 font-bold italic max-lg:px-4">
+      <h1 className="text-lg md:text-2xl lg:text-3xl absolute text-white z-30 font-bold italic max-lg:px-4">
         Your ticket to endless entertainment! Start exploring a world of movies.
       </h1>
     </div>
