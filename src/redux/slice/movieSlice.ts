c6b676615be624524be8476e3baf97dcd4ef5af7
Mustafa Keser <keserzade@gmail.com>
2024-12-15 20:46:12 +0300

    completed f

--- src/redux/slice/movieSlice.ts
+++ src/redux/slice/movieSlice.ts
@@ -7,7 +7,7 @@ export const movieApi = createApi({
     prepareHeaders: (headers) => {
       headers.set(
         'Authorization',
-        `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`
+        `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZGQ5ZDUwOTc3ODY5YWMzYzEwMzBiNzhjNWRjMTljNCIsIm5iZiI6MTczMzgzNDk3My43ODIsInN1YiI6IjY3NTgzOGRkNDYwNDQ3YjA5ZGU0MGRhNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qcZloGMKejdXdvSjPDW90AKJEGiIp-r5oBhUTMHS5oA`
       );
       return headers;
     },
