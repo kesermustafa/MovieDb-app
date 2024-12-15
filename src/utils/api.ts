7bc521836d805d4086f300384adcd402febf0d27
Mustafa Keser <keserzade@gmail.com>
2024-12-15 20:43:16 +0300

    completed

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,10 +3,10 @@ const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 const api = axios.create({
-  baseURL: `${BASE_URL}`,
+  baseURL: `https://api.themoviedb.org/3`,
   headers: {
     accept: 'application/json',
-    Authorization: `Bearer ${API_KEY}`,
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZGQ5ZDUwOTc3ODY5YWMzYzEwMzBiNzhjNWRjMTljNCIsIm5iZiI6MTczMzgzNDk3My43ODIsInN1YiI6IjY3NTgzOGRkNDYwNDQ3YjA5ZGU0MGRhNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qcZloGMKejdXdvSjPDW90AKJEGiIp-r5oBhUTMHS5oA`,
   },
 });
 
